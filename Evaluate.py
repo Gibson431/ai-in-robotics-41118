f480d0822faef7389be3f249e5c029fd9a58c4cd
Tim Gibson <timmaxgibson@gmail.com>
2024-04-24 17:13:30 +1000

    update eval to do a full run (~40sec)

--- Evaluate.py
+++ Evaluate.py
@@ -3,5 +3,10 @@ import time
 
 env = RandomTrackEnv(render_mode="tp_camera")
 
-while True:
-    time.sleep(1)
+done = False
+while not done:
+    action = env.action_space.sample()
+    state_, reward, done, _info = env.step(action)
+    _ = env.render("fp_camera")
+
+env.close()
