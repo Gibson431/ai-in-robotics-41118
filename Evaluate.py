c0b843c9050b863518493551dc89eb0d4a5b85a5
Tim Gibson <timmaxgibson@gmail.com>
2024-04-24 15:21:54 +1000

    cleanup eval file

--- Evaluate.py
+++ Evaluate.py
@@ -1,27 +1,5 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import gym
-import torch
-import agents
-import time
 from fsae.envs import *
-import sys
-import gym
-import pybullet as p
-import matplotlib.pyplot as plt
-from IPython import display as ipythondisplay
-from pyvirtualdisplay import Display
-from IPython.display import HTML
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import numpy as np
-import math
-from collections import defaultdict
-import pickle
-from IPython.display import clear_output
-import torch
-import random
+import time
 
 env = RandomTrackEnv(render_mode="tp_camera")
 
