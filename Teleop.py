59139aecfb5f4e4dc7e637a8b8c7d44fed86aed2
ConnorKeogh <Connor.keogh@student.uts.edu.au>
2024-05-01 18:52:31 +1000

    updates for usability.

--- Teleop.py
+++ Teleop.py
@@ -1,43 +1,50 @@
-from fsae.envs import *
-import time
-import sys
+import msvcrt
+from fsae.envs import RandomTrackEnv
 
-env = RandomTrackEnv(render_mode="tp_camera")
+def get_key(): 
+    key = None
+    # Check if there's data available to read from sys.stdin without blocking
+    if msvcrt.kbhit():
+        key = msvcrt.getch().decode()
+    return key
 
-done = False
-print("started")
-while not done:
-    key = sys.stdin.read(1)
-    print(f"{key}\n")
-    action = []
-    if key == "q":  # Move forward-left
-        action = [1, -0.6]
-    elif key == "w":  # Move forward
-        action = [1, 0.0]
-    elif key == "e":  # Move forward-right
-        action = [1, 0.6]
-    elif key == "a":  # Steer left (no throttle)
-        action = [0, -0.6]
-    elif key == "s":  # No steer / throttle
-        action = [0, 0]
-    elif key == "d":  # Steer right (no throttle)
-        action = [0, 0.6]
-    elif key == "z":  # Move back-left
-        action = [-1, -0.6]
-    elif key == "x":  # Move back
-        action = [-1, 0.0]
-    elif key == "c":  # Move back-right
-        action = [-1, 0.6]
-    elif key == "p":  # quit application
-        break
-    else:
-        key = "No input"
-        action = [0, 0]
+def main():
+    env = RandomTrackEnv(render_mode="tp_camera")
+    done = False
+    print("started")
+    action = [0,0]
 
-    if key != "No input":
-        print(f"{key} recognised. Action: ", action)
+    while not done:
+        key = get_key() # This is a non-blocking call now
+        if key is not None:
+            if key == "q":  # Move forward-left
+                action = [1, -0.6]
+            elif key == "w":  # Move forward
+                action = [1, 0.0]
+            elif key == "e":  # Move forward-right
+                action = [1, 0.6]
+            elif key == "a":  # Steer left (no throttle)
+                action = [0, -0.6]
+            elif key == "s":  # No steer / throttle
+                action = [0, 0]
+            elif key == "d":  # Steer right (no throttle)
+                action = [0, 0.6]
+            elif key == "z":  # Move back-left
+                action = [-1, -0.6]
+            elif key == "x":  # Move back
+                action = [-1, 0.0]
+            elif key == "c":  # Move back-right
+                action = [-1, 0.6]
+            elif key == "p":  # quit application
+                break
+            print(f"{key} recognised. Action: ", action)
+        
+        #If no key is pressed, pass the same command
         state_, reward, done, _info = env.step(action)
         _ = env.render("fp_camera")
-    print("tick")
+        print("tick")
 
-env.close()
+    env.close()
+
+if __name__ == "__main__":
+    main()
