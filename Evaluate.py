c1bbf281e75ee1c736821b55c826cb9fc8182a94
Gibson431 <timmaxgibson@gmail.com>
2024-05-05 21:13:55 +1000

    fix debug msgs

--- Evaluate.py
+++ Evaluate.py
@@ -9,9 +9,8 @@ total_reward = 0
 while not done:
     action = env.action_space.sample()
     state_, reward, done, _info = env.step(action)
-    print(state_)
     total_reward += reward
-    # print(f"total: {total_reward}, step: {reward}")
+    print(f"total: {total_reward}, step: {reward}")
     _ = env.render("fp_camera")
 
 env.close()
